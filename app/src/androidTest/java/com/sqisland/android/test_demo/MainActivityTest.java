2d6588b47261f7861b406bb9b4f92fc8b2d03f4a
Chiu-Ki Chan <chiuki@gmail.com>
2015-04-21 22:44:26 -0600

    Use Espresso 2.1 ActivityTestRule

--- app/src/androidTest/java/com/sqisland/android/test_demo/MainActivityTest.java
+++ app/src/androidTest/java/com/sqisland/android/test_demo/MainActivityTest.java
@@ -1,16 +1,22 @@
 package com.sqisland.android.test_demo;
 
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.joda.time.DateTime;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import dagger.Component;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -22,25 +28,35 @@ public class MainActivityTest {
   @Inject
   Clock clock;
 
-  protected ActivityLauncher launcher;
+  @Singleton
+  @Component(modules = MockClockModule.class)
+  public interface TestComponent extends DemoComponent {
+    void inject(MainActivityTest mainActivityTest);
+  }
+
+  @Rule
+  public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(
+      MainActivity.class,
+      true,     // initialTouchMode
+      false);   // launchActivity. False so we can customize the intent per test method
 
   @Before
   public void setUp() {
-    launcher = new ActivityLauncher(MainActivity.class);
-    ((ActivityLauncher.TestComponent) launcher.getApplication().component()).inject(this);
-  }
-
-  @After
-  public void tearDown() {
-    launcher.tearDown();
-    launcher = null;
+    Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+    DemoApplication app
+        = (DemoApplication) instrumentation.getTargetContext().getApplicationContext();
+    TestComponent component = DaggerMainActivityTest_TestComponent.builder()
+        .mockClockModule(new MockClockModule())
+        .build();
+    app.setComponent(component);
+    component.inject(this);
   }
 
   @Test
   public void today() {
     Mockito.when(clock.getNow()).thenReturn(new DateTime(2008, 9, 23, 0, 0, 0));
 
-    launcher.getActivity();
+    activityRule.launchActivity(new Intent());
 
     onView(withId(R.id.date))
         .check(matches(withText("2008-09-23")));
@@ -48,15 +64,11 @@ public class MainActivityTest {
 
   @Test
   public void intent() {
-    launcher.setLaunchIntentModifier(new ActivityLauncher.IntentModifier() {
-      @Override
-      public void modify(Intent intent) {
-        DateTime dateTime = new DateTime(2014, 10, 15, 0, 0, 0);
-        intent.putExtra(MainActivity.KEY_MILLIS, dateTime.getMillis());
-      }
-    });
+    DateTime dateTime = new DateTime(2014, 10, 15, 0, 0, 0);
+    Intent intent = new Intent();
+    intent.putExtra(MainActivity.KEY_MILLIS, dateTime.getMillis());
 
-    launcher.getActivity();
+    activityRule.launchActivity(intent);
 
     onView(withId(R.id.date))
         .check(matches(withText("2014-10-15")));
